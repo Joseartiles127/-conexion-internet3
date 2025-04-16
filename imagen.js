45e01ab40440273ea2b46c10b8646e09c2a802e4
Joseartiles127 <richiezespiritusanto062@gmail.com>
2025-04-16 16:28:45 -0400

    Update imagen.js

--- imagen.js
+++ imagen.js
@@ -1,1 +1,11 @@
 console.log("Proyecto cargado correctamente - Conexión a Internet");
+function ampliarImagen(imagen) {
+  const modal = document.getElementById("modal");
+  const imagenGrande = document.getElementById("imagenGrande");
+  imagenGrande.src = imagen.src;
+  modal.style.display = "block";
+}
+
+function cerrarModal() {
+  document.getElementById("modal").style.display = "none";
+}
